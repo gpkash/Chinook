1f50dd30ba5bbeca1559bc0af6b8fa391aa6eeaa
Gary Kash <capikaw@gmail.com>
2020-03-22 11:51:03 -0600

    Separated out any coding key to it’s own file.

--- Chinook/Classes/Model/Decoding Strategies/AnyCodingKey.swift
+++ Chinook/Classes/Model/Decoding Strategies/AnyCodingKey.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+// wrapper to allow us to substitute our mapped string keys.
+struct AnyCodingKey : CodingKey {
+
+  var stringValue: String
+  var intValue: Int?
+
+  init(_ base: CodingKey) {
+    self.init(stringValue: base.stringValue, intValue: base.intValue)
+  }
+
+  init(stringValue: String) {
+    self.stringValue = stringValue
+  }
+
+  init(intValue: Int) {
+    self.stringValue = "\(intValue)"
+    self.intValue = intValue
+  }
+
+  init(stringValue: String, intValue: Int?) {
+    self.stringValue = stringValue
+    self.intValue = intValue
+  }
+}
